b2d52d529b5457d457edfe2dd0196c8021d43447
PrantoshB <pbiswas1007@gmail.com>
2023-05-17 00:30:50 +0530

    Fix Linters Errors

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::GreetingsController < ApplicationController
   def index
     @greetings = Greeting.all
-    render json: @greetings, status: :ok 
-end
-    
+    render json: @greetings, status: :ok
+  end
 end
