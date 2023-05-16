8b07767cb89486ce9036ff173fb876d11c0f5a3b
PrantoshB <pbiswas1007@gmail.com>
2023-05-17 00:27:03 +0530

    Create database and migrations

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+Greeting.create(greeting: "Hello, World!")
+Greeting.create(greeting: "Ciao, Mondo!")
+Greeting.create(greeting: "Hola, Mundo!")
+Greeting.create(greeting: "Hello, Wêreld!")
+Greeting.create(greeting: "Hallo, Welt!")
